b1912ab26e149a4eb8cd9ac58412d1bd60756315
mlpeck <mlpeck54@earthlink.net>
2021-07-06 12:18:59 -0500

    sconic adds argument 'eps' and should work with obstructed pupils

--- R/sa_astig_null.r
+++ R/sa_astig_null.r
@@ -41,8 +41,7 @@ zconic <- function(D, rc, b = -1, lambda = 632.8, nmax = 6) {
 ## Zernike coefficients of Wavefront error due to testing a conic at center of curvature
 ## this should be more accurate for numerical nulling than twice the height difference as in zconic
 
-sconic <- function(D, rc, b = -1, lambda = 632.8, nmax = 6) {
-    require(zernike)
+sconic <- function(D, rc, b = -1, eps = 0., lambda = 632.8, nmax = 6) {
     
     if ((nmax %%2) != 0) stop("nmax must be even")
     nz <- (nmax-2)/2
@@ -54,11 +53,11 @@ sconic <- function(D, rc, b = -1, lambda = 632.8, nmax = 6) {
         (rho*na)^2/(1 + sqrt(1 - (1+b)*(rho*na)^2))^2)
     }
     q <- function(rho, na, b) 1 - sqrt(1 + p(rho, na, b))
-    f <- function(rho, na, b, n) q(rho, na, b)*rzernike(rho, n, 0)*rho
+    f <- function(rho, na, b, n) q(rho, na, b)*rzernike(sqrt((rho^2-eps^2)/(1-eps^2)), n, 0)*rho
     for (i in 1:nz) {
         n <- 2 + 2*i
-        int <- integrate(f, lower=0, upper=1, na=na, b=b, n=n)
-        zc[i] <- 4 * rc * sqrt(n+1) * int$value * 1.e6/lambda
+        int <- integrate(f, lower=eps, upper=1, na=na, b=b, n=n)
+        zc[i] <- 4 * rc * sqrt(n+1) * int$value * 1.e6/lambda/(1-eps^2)
         if (int$message != "OK") warning("coefficient may be inaccurate")
     }
     zc
